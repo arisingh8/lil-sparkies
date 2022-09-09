67514363459b76391143346e01fbfbad13408e60
Ari Singh <arisingh8@gmail.com>
2022-09-09 16:13:17 -0400

    very broken, but WS implementation in progress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,205 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+#[allow(unused_imports)]
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::path::PathBuf;
+use std::sync::{Condvar, Mutex};
+use std::{cell::RefCell, env, sync::atomic::*, sync::Arc, thread, time::*};
 
-fn main() {
+use anyhow::{Context as _, Result, bail};
+
+use log::*;
+
+use async_tungstenite::WebSocketStream;
+use futures::sink::{Sink, SinkExt};
+use smol::{future, prelude::*, Async};
+use tungstenite::Message;
+
+use embedded_hal::digital::v2::OutputPin;
+
+use embedded_svc::wifi::*;
+
+use esp_idf_svc::netif::*;
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::sysloop::*;
+use esp_idf_svc::wifi::*;
+
+use esp_idf_hal::prelude::*;
+
+use esp_idf_sys::{self, c_types};
+use esp_idf_sys::{esp, EspError};
+
+fn main() -> Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
     println!("Hello, world!");
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    #[allow(unused)]
+    let peripherals = Peripherals::take().unwrap();
+    #[allow(unused)]
+    let pins = peripherals.pins;
+
+    #[allow(unused)]
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    #[allow(unused)]
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+    #[allow(unused)]
+    let default_nvs = Arc::new(EspDefaultNvs::new()?);
+
+    // Turn on onboard LED
+    let mut onboard_led = pins.gpio2.into_output()?;
+    onboard_led.set_high()?;
+
+    let mut wifi = wifi(
+        netif_stack.clone(),
+        sys_loop_stack.clone(),
+        default_nvs.clone(),
+    )?;
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    Ok(())
+}
+
+fn wifi(
+    netif_stack: Arc<EspNetifStack>,
+    sys_loop_stack: Arc<EspSysLoopStack>,
+    default_nvs: Arc<EspDefaultNvs>,
+) -> Result<Box<EspWifi>> {
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+
+    info!("Wifi created, about to setup AP");
+
+    let conf = Configuration::AccessPoint(AccessPointConfiguration {
+        ssid: "esp_tester".into(),
+        channel: 1,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&conf)?;
+
+    info!("Wifi configuration completed, about to wait for status");
+
+    wifi.wait_status_with_timeout(Duration::from_secs(20), |status| !status.is_transitional())
+        .map_err(|e| anyhow::anyhow!("Unexpected Wifi status: {:?}", e))?;
+
+    let status = wifi.get_status();
+
+    if let Status(ClientStatus::Stopped, ApStatus::Started(ApIpStatus::Done)) = status {
+        info!("AP started");
+    } else {
+        bail!("Unexpected Wifi status: {:?}", status);
+    }
+
+    Ok(wifi)
+}
+
+fn test_tcp_bind() -> Result<()> {
+    fn test_tcp_bind_accept() -> Result<()> {
+        info!("About to bind a simple echo service to port 8080");
+
+        let listener = TcpListener::bind("0.0.0.0:8080")?;
+        let host = format!("wss://{}", listener.local_addr()?);
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    info!("Accepted client: {}", stream.peer_addr()?);
+                    let stream = WsStream::Plain(tungstenite::accept(stream).await?);
+
+                    thread::spawn(move || {
+                        test_tcp_bind_handle_client(stream);
+                    });
+                }
+                Err(e) => {
+                    error!("Error: {}", e);
+                }
+            }
+        }
+
+        unreachable!()
+    }
+
+    fn test_tcp_bind_handle_client(mut stream: WsStream) {
+        // read 20 bytes at a time from stream echoing back to stream
+        loop {
+            match stream.next() {
+                Ok(n) => {
+                    if n.into_text().unwrap().len() == 0 {
+                        // connection was closed
+                        break;
+                    }
+                    stream.write_all(&read[0..n]).unwrap();
+                }
+                Err(err) => {
+                    panic!("{}", err);
+                }
+            }
+        }
+    }
+
+    thread::spawn(|| test_tcp_bind_accept().unwrap());
+
+    Ok(())
+}
+
+/// A WebSocket or WebSocket+TLS connection.
+enum WsStream {
+    /// A plain WebSocket connection.
+    Plain(WebSocketStream<TcpStream>),
+
+    // A WebSocket connection secured by TLS.
+    // Tls(WebSocketStream<TlsStream<Async<TcpStream>>>),
+}
+
+impl Sink<Message> for WsStream {
+    type Error = tungstenite::Error;
+
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        match &mut *self {
+            WsStream::Plain(s) => Pin::new(s).poll_ready(cx),
+            //WsStream::Tls(s) => Pin::new(s).poll_ready(cx),
+        }
+    }
+
+    fn start_send(mut self: Pin<&mut Self>, item: Message) -> Result<(), Self::Error> {
+        match &mut *self {
+            WsStream::Plain(s) => Pin::new(s).start_send(item),
+            //WsStream::Tls(s) => Pin::new(s).start_send(item),
+        }
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        match &mut *self {
+            WsStream::Plain(s) => Pin::new(s).poll_flush(cx),
+            //WsStream::Tls(s) => Pin::new(s).poll_flush(cx),
+        }
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        match &mut *self {
+            WsStream::Plain(s) => Pin::new(s).poll_close(cx),
+            //WsStream::Tls(s) => Pin::new(s).poll_close(cx),
+        }
+    }
+}
+
+impl Stream for WsStream {
+    type Item = tungstenite::Result<Message>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        match &mut *self {
+            WsStream::Plain(s) => Pin::new(s).poll_next(cx),
+            //WsStream::Tls(s) => Pin::new(s).poll_next(cx),
+        }
+    }
 }
